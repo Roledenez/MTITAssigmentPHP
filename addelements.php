d6078f5019e30868527e8f9801bb8bc3f2e3a67f
Lakna Ariyathilaka <Lakna Ariyathilaka>
2016-02-10 22:16:17 +0530

    Form done

--- addelements.php
+++ addelements.php
@@ -22,3 +22,9 @@ $db->insertElement("textBox",\app\Element::text(array(
 
 $db->insertElement("button",\app\Element::button("Button"),0);
 $db->insertElement("link",\app\Element::buttonLink("Button","add.php"),0);
+$db->insertElement("RadioButton",\app\Element::radioButton("Radiobtn",array
+(0 => 'radio 1')),0);
+$db->insertElement("CheckBox",\app\Element::checkBox("Check",array(0 =>
+    'check 1',1 => 'check 2')),0);
+$db->insertElement("Dropdown",\app\Element::dropdownList("Drop",array(0 =>
+    'op 1',1 => 'op 2',2 => 'op 3')),0);
